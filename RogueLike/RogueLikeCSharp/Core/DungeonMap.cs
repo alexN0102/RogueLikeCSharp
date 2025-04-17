b215085fa0592e04b44002fd62301e057676b208
alexN0102 <alexander.l.novelli@gmail.com>
2025-04-17 09:38:30 -0400

    First commit

---
+++ RogueLike/RogueLikeCSharp/Core/DungeonMap.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+using RogueSharp;
+using RogueSharpV3Tutorial.Core;
+
+namespace RogueLikeCSharp.Core
+{
+    // Our custom DungeonMap class extends the base RogueSharp Map Class
+    public class DungeonMap : Map
+    {
+        public void Draw(RLConsole mapConsole)
+        {
+            mapConsole.Clear();
+            foreach(Cell cell in GetAllCells())
+            {
+                SetConsoleSymbolForCell(mapConsole, cell);
+            }
+        }
+        private void SetConsoleSymbolForCell(RLConsole console, Cell cell)
+        {
+            //When we havent explored a cell yet, we dont want to draw anything
+            if (!cell.IsExplored)
+            {
+                return;
+            }
+            // When a cell is currently in the field of view it should be drawn with lighter colors
+            if (IsInFov(cell.X, cell.Y))
+            {
+                // Choose the symbol to draw based on if the cell is walkable or not
+                if(cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.FloorFov, Colors.FloorBackgroundFov, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Colors.WallFov, Colors.WallBackgroundFov, '#');
+                }
+            }
+            else
+            {
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.Floor, Colors.FloorBackground, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Colors.Wall, Colors.WallBackground, '#');
+                }
+            }
+        }
+    }
+}
